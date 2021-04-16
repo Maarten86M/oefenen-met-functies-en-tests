1087324df6bb171f0767be34d650e3cc29a0526f
Maarten86M <maartenmoelands@gmail.com>
2021-04-16 11:48:59 +0200

    simpele test geschreven in test bestand

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,22 @@
 // Leeg test bestand
 
-// Arrange
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+test("telt 2 sommen op en geeft dan een uitkomst hiervan",function () {
+    // Arrange
+    const number1 = 4;
+    const number2 = 8;
+
+    // Act
+    const total = sum(number1, number2);
+
+    // Assert (bewering)
+    expect(total).toBe(12);
+})
+
+
 
 // Act
 
